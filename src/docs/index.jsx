31f67ebea97a5309ce49beab7060b94736e16c20
lambdaAgent <alfredovidy@gmail.com>
2018-11-16 17:32:28 +0700

    split the calendar into mobile and desktop \n The mobile version is not complete

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -6,13 +6,8 @@ import "./styles.css";
 function Demo() {
   const time = ['07:00AM', '07:30AM', '08:00AM', '08:30AM', '09:00AM', '09:30AM', '10:00AM', '11:00AM', '12:309M', '01:00PM']
   return (
-    <div style={{width: 1000, margin: '0 auto', backgroundColor: 'white', padding: 20}}>
-      <div style={{display: 'flex', marginBottom: 20}}>
-        <div style={{width: 638}}>Choose A Date</div>
-        <div style={{marginLeft: 37}}>Choose A Time</div>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <Calendar
+    <div>
+        <Calendar isMobile={false}
           listOfDates={
             [{date: '01/11/2018', surgePrice: 1000}, {date: '15/11/2018', surgePrice: 100}, {date: '01/12/2018', surgePrice: 2000}, {date: '10/10/2018'}]
           } 
@@ -20,13 +15,6 @@ function Demo() {
             time.map(t => ({time: t, surgePrice: null}))
           }
         />
-      </div>
-      <div>
-        <div style={{borderTop: '1px solid rgb(0,0,0,0.2)'}}/>
-        <div>
-          <button>confirm</button>
-        </div>
-      </div>
     </div>
   );
 }
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,2 +1,74 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import Calendar from './Calendar/Calendar'
-export default Calendar
+
+class DesktopCalendar  extends React.PureComponent{
+    static propTypes = {
+        listOfDates: PropTypes.arrayOf(
+            PropTypes.shape({
+                date: PropTypes.string,
+                surgePrice: PropTypes.number
+            })
+        ),
+        listOfTimes: PropTypes.arrayOf(
+            PropTypes.shape({
+                time: PropTypes.string,
+                surgePrice: PropTypes.number
+            })
+        )
+    }
+
+    render(){
+        return (
+            <div style={{width: 1000, margin: '0 auto', backgroundColor: 'white', padding: 20}}>
+              <div style={{display: 'flex', marginBottom: 20}}>
+                <div style={{width: 638}}>Choose A Date</div>
+                <div style={{marginLeft: 37}}>Choose A Time</div>
+              </div>
+              <div style={{display: 'flex', justifyContent: 'center'}}>
+                <Calendar
+                  listOfDates={this.props.listOfDates || []} 
+                  listOfTimes={this.props.listOfTimes || []}
+                />
+              </div>
+              <div>
+                <div style={{borderTop: '1px solid rgb(0,0,0,0.2)'}}/>
+                <div>
+                  <button>confirm</button>
+                </div>
+              </div>
+            </div>
+          );
+    }
+}
+
+const MobileCalendar = DesktopCalendar
+
+class Container extends React.PureComponent {
+    static propTypes = {
+        isMobile: PropTypes.bool,
+        listOfDates: PropTypes.arrayOf(
+            PropTypes.shape({
+                date: PropTypes.string,
+                surgePrice: PropTypes.number
+            })
+        ),
+        listOfTimes: PropTypes.arrayOf(
+            PropTypes.shape({
+                time: PropTypes.string,
+                surgePrice: PropTypes.number
+            })
+        )
+    }
+    render(){
+        const { isMobile } = this.props
+        return (
+            <div>
+                {isMobile ? <MobileCalendar {...this.props} /> : <DesktopCalendar {...this.props} />}
+            </div>
+        )
+    }
+}
+
+
+export default Container
